82cabe43477bba0309a5160e77adae989e73e3ec
VirxEC <virx@virxcase.dev>
2023-06-16 23:36:16 -0400

    Fix bug

--- agent.py
+++ agent.py
@@ -7,8 +7,8 @@ from stable_baselines3 import PPO
 class Agent:
     def __init__(self):
         base_folder = os.path.dirname(os.path.realpath(__file__))
-        self.actor = PPO.load(os.path.join(base_folder, "VirxEAI.zip"))
+        self.actor: PPO = PPO.load(os.path.join(base_folder, "VirxEAI.zip"))
 
     def act(self, state):
         with torch.no_grad():
-            return self.actor(state)[0]
+            return self.actor.predict(state)[0]
